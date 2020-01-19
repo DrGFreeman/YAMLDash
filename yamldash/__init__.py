2d7e0a467ac3d0780c1b01cfa5c9ac973928d8d1
DrGFreeman <DrGFreeman@users.noreply.github.com>
2020-01-19 09:37:13 -0500

    Add app title

--- yamldash/__init__.py
+++ yamldash/__init__.py
@@ -6,6 +6,8 @@ from yamldash.layout import theme
 
 app = dash.Dash(__name__, external_stylesheets=[theme])
 
+app.title = "YAMLDash - Interactive YAML Validation"
+
 app.layout = layout
 
 import yamldash.callbacks  # noqa: E402, F401
